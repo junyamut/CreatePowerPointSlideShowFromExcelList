7089e05753214b0bed8f8904a565f2c1c3e588c7
Jose Yamut <junyamut@users.noreply.github.com>
2019-05-09 04:17:37 +0800

    Removed unused imports & class field/declaration

--- src/main/java/xyz/joseyamut/app/ReadTemplate.java
+++ src/main/java/xyz/joseyamut/app/ReadTemplate.java
@@ -5,12 +5,8 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import org.apache.poi.xslf.usermodel.XMLSlideShow;
 import org.apache.poi.xslf.usermodel.XSLFSlide;
-import org.apache.xmlbeans.XmlObject;
-import org.apache.xmlbeans.XmlString;
-import org.openxmlformats.schemas.presentationml.x2006.main.CTSlide;
 
 public class ReadTemplate extends TemplateInputPath {
-	private String delimiter; 
 	private FileInputStream fileInputStream;
 	private XMLSlideShow template;
 	private XSLFSlide masterSlide;
@@ -20,7 +16,6 @@ public class ReadTemplate extends TemplateInputPath {
 			fileInputStream = new FileInputStream(new File(getPath().toString()));
 			template = new XMLSlideShow(fileInputStream);
 			masterSlide = template.getSlides().get(0);
-			delimiter = Properties.getProperties().getApp().getDatSrcAttr().getDelimiter();
 		} catch (IOException e) { 
 		} finally { 
 			try {
